2dceffdece4dda8e7d1d827194382fa18c3f3588
cdkw2 <67598073+cdkw2@users.noreply.github.com>
2023-07-21 23:27:24 +0530

    added all 8 gens starters

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
 import random
+import os
 
 class Pokemon:
     def __init__(self, name, type, level, max_hp, current_hp, attack, defense, speed):
@@ -20,23 +27,82 @@ class Pokemon:
         return self.current_hp == 0
     
     def attack_opponent(self, opponent):
+        is_critical = random.random() < 0.25  # 25% chance of a critical hit, adjust as desired
         damage = self.attack - opponent.defense
+        type_chart = {
+            "Grass": {"Water": 2.0, "Fire": 0.5, "Electric": 1.0},
+            "Water": {"Fire": 2.0, "Grass": 0.5, "Electric": 0.5},
+            "Fire": {"Water": 0.5, "Grass": 2.0, "Electric": 1.0},
+            "Electric": {"Water": 2.0, "Grass": 1.0, "Fire": 1.0}
+        }
+
+        effectiveness = type_chart.get(self.type, {}).get(opponent.type, 1.0)
+        damage *= effectiveness
+
+        if is_critical:
+            print("Critical Hit!")
+            damage *= 2  # Double the damage for a critical hit
+
         if damage < 1:
             damage = 1
+
         opponent.take_damage(damage)
         print(f"{self.name} attacked {opponent.name} and dealt {damage} damage!")
+        if effectiveness > 1.0:
+            print("It's super effective!")
+        elif effectiveness < 1.0:
+            print("It's not very effective.")
+        
         if opponent.is_fainted():
             print(f"{opponent.name} fainted!")
     
     def __str__(self):
         return f"{self.name} (lvl {self.level}) with {self.current_hp} / {self.max_hp} HP"
 
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+    
 # Define the available Pokemon
 pokemons = [
+    # First Generation
     Pokemon("Bulbasaur", "Grass", 5, 45, 45, 49, 49, 45),
     Pokemon("Charmander", "Fire", 5, 39, 39, 52, 43, 65),
     Pokemon("Squirtle", "Water", 5, 44, 44, 48, 65, 43),
-    Pokemon("Pikachu", "Electric", 5, 35, 35, 55, 30, 90)
+    
+    # Second Generation
+    Pokemon("Chikorita", "Grass", 5, 45, 45, 49, 65, 45),
+    Pokemon("Cyndaquil", "Fire", 5, 39, 39, 52, 43, 65),
+    Pokemon("Totodile", "Water", 5, 50, 50, 48, 65, 43),
+    
+    # Third Generation
+    Pokemon("Treecko", "Grass", 5, 40, 40, 45, 35, 70),
+    Pokemon("Torchic", "Fire", 5, 45, 45, 60, 40, 45),
+    Pokemon("Mudkip", "Water", 5, 50, 50, 70, 50, 40),
+
+    # Fourth Generation
+    Pokemon("Turtwig", "Grass", 5, 55, 55, 68, 64, 31),
+    Pokemon("Chimchar", "Fire", 5, 44, 44, 58, 44, 61),
+    Pokemon("Piplup", "Water", 5, 53, 53, 51, 53, 40),
+
+    # Fifth Generation
+    Pokemon("Snivy", "Grass", 5, 45, 45, 45, 55, 63),
+    Pokemon("Tepig", "Fire", 5, 65, 65, 63, 45, 45),
+    Pokemon("Oshawott", "Water", 5, 55, 55, 55, 45, 45),
+
+    # Sixth Generation
+    Pokemon("Chespin", "Grass", 5, 56, 56, 61, 65, 38),
+    Pokemon("Fennekin", "Fire", 5, 40, 40, 62, 52, 60),
+    Pokemon("Froakie", "Water", 5, 41, 41, 56, 40, 71),
+
+    # Seventh Generation
+    Pokemon("Rowlet", "Grass", 5, 68, 68, 55, 50, 42),
+    Pokemon("Litten", "Fire", 5, 45, 45, 65, 40, 70),
+    Pokemon("Popplio", "Water", 5, 50, 50, 54, 54, 40),
+
+    # Eighth Generation
+    Pokemon("Grookey", "Grass", 5, 50, 50, 65, 50, 65),
+    Pokemon("Scorbunny", "Fire", 5, 50, 50, 71, 40, 69),
+    Pokemon("Sobble", "Water", 5, 50, 50, 40, 40, 70),
 ]
 
 # Choose a random player and opponent Pokemon
@@ -49,6 +115,7 @@ print("I choose you, {}!".format(player_pokemon.name))
 # Start the battle
 while True:
     # Player's turn
+    cls() # Clear the console
     print()
     print(player_pokemon)
     print(opponent_pokemon)
@@ -69,6 +136,7 @@ while True:
         continue
     
     # Opponent's turn
+    clear()  # Clear the console
     print()
     print(player_pokemon)
     print(opponent_pokemon)
@@ -76,3 +144,4 @@ while True:
     if player_pokemon.is_fainted():
         print("You lost the battle!")
         break
+
